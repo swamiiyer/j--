4a2766944b55327e77e220604e9e1024a554551c
swamiiyer <swami.iyer@gmail.com>
2025-01-25 09:08:59 +0530

    Added enclosing statement support

--- src/jminusminus/JMember.java
+++ src/jminusminus/JMember.java
@@ -1,9 +1,17 @@
 package jminusminus;
 
+import java.util.Stack;
+
 /**
  * An interface supported by all class (or later, interface) members.
  */
 interface JMember {
+    /**
+     * Used to identify the enclosing control-flow statement of a break/continue statement.
+     */
+    static Stack<JStatement> enclosingStatement = new Stack<JStatement>();
+
+
     /**
      * Declares the member names in the specified (class) context and generates the member headers in the partial class.
      *
